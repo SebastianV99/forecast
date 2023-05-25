ee9154d04ae1d0607fa2ea5ab3b3587b1a0d0502
SebastianV99 <vollmer.sebastian@outlook.de>
2023-05-25 17:26:49 +0200

    klick auf ibk simuliert

--- main.js
+++ main.js
@@ -38,6 +38,30 @@ async function showForecast(url, latlng) {
 
     let current = jsondata.properties.timeseries[0].data.instant.details;
     console.log(current);
+
+    let timestamp = new Date(jsondata.properties.meta.updated_at).toLocaleString();
+    let timeseries = jsondata.properties.timeseries;
+
+    let marker = `
+    <h4> Aktuelles Wetter für ${latlng.lat.toFixed(4)}, ${latlng.lng.toFixed(4)} (${timestamp})</h4>
+    <table>
+        <tr><td>Luftdruck in hPa:</td><td>${current.air_pressure_at_sea_level}</td></tr>
+        <tr><td>Lufttemperatur in °C:</td><td>${current.air_temperature}</td></tr>
+        <tr><td>Bewölungsgrad in %:</td><td>${current.cloud_area_fraction}</td></tr>
+        <tr><td>Relative Feuchte</td><td>${current.relative_humidity}</td></tr>
+        <tr><td>Windrichtung in Grad</td><td>${current.wind_from_direction}</td></tr>
+        <tr><td>Windgeschwindigkeit in km/h:</td><td>${current.wind_speed}</td></tr>
+    </table>
+    `;
+    //Wettersymbol
+    for (let i=0; i<=24; i+=3){
+        console.log(timeseries[i]);
+        let icon = timeseries[i].data.next_1_hours.summary.symbol_code;
+        let image = `icons/${icon}.svg`;
+        marker += `<img src="${image}" style="width:32px;" title="${timeseries[i].time.toLocaleString()}">`
+        console.log(icon,image);
+    }
+    L.popup().setLatLng(latlng).setContent(marker).openOn(map);
 }
 
 //auf Kartenklick reagieren
@@ -46,3 +70,8 @@ map.on("click", function(evt) {
     let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`;
     showForecast(url, evt.latlng);
 });
+
+//klick auf ibk simulieren
+map.fireEvent ("click", {
+    latlng: L.latLng(ibk.lat, ibk.lng)
+})
